a6b7da59227106ed911b65cc7c4c982b63b5b512
mspasinski <59970663+mspasinski@users.noreply.github.com>
2020-01-16 18:18:46 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-ss
+// ssssssss
+
+require (["esri/Map", "esri/views/MapView"],
+   function(Map, MapView)
+   {
+    let map1 = new Map({basemap:"streets"});
+
+    let mapContainer = new MapView({
+        container: "d",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
+        map: map1  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
+        , zoom:16,
+        center: [22.57, 51.25],
+     }); 
+   })
